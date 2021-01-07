77bb1b4aca4b0a0b00d844894d97dc743d50bc63
Kieran Huon <kieran.huon@devinci.fr>
2021-01-07 16:24:23 +0100

    modification

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include <stdio.h>
 
 int main()
@@ -9,14 +9,4 @@ int main()
 return 0;
 
 }
-=======
-#include <stdio.h>
 
-int main()
-{
-    for(int i=1; i<=10; i++){
-    printf("%d\n",i);
-}
-    return 0;
-}
->>>>>>> 2b642fac9be9ba189f7c796527e50f33053abbae
